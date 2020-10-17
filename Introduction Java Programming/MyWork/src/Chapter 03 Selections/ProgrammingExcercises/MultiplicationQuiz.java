f1091b68cb82f15491ae064ed4e238a0a726c5db
Kim Van de Casteele <Kinkyk013@gmail.com>
2020-10-17 12:01:09 +0200

    Chapters 1 - 2 - 3

--- Introduction Java Programming/MyWork/src/Chapter 03 Selections/ProgrammingExcercises/MultiplicationQuiz.java
+++ Introduction Java Programming/MyWork/src/Chapter 03 Selections/ProgrammingExcercises/MultiplicationQuiz.java
@@ -1,8 +1,25 @@
 package com.company.Chapter3Selections.ProgrammingExcercises;
 
 public class MultiplicationQuiz {
-    /* SubtractionQuiz randomly generates a subtraction question. Revise the program to randomly generate a
+    /* SubtractionQuiz.java randomly generates a subtraction question. Revise the program to randomly generate a
     multiplication question with two integers less than 1000.
      */
+    public static void main(String[] args) {
+        // Generate two random single-digit integers
+        int number1 = (int) (Math.random() * 100);
+        int number2 = (int) (Math.random() * 100);
 
+        // Ask the user to answer te question
+        System.out.println("What is " + number1 + " * " + number2 + "?");
+        Scanner input = new Scanner(System.in);
+        int answer = input.nextInt();   // get answer
+
+        // Grade the answer and display the result
+        if (number1 * number2 == answer)        // check the answer
+            System.out.println("You are correct!");
+        else {
+            System.out.println("Your answer is wrong.");
+            System.out.println(number1 + " * " + number2 + " should be " + (number1 * number2));
+        }
+    }
 }
