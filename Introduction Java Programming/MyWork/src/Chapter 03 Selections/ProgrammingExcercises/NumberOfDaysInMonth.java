fddfa733099232d9f53f1e8521f399aae3e7c621
KinkyK013 <KinkyK013@gmail.com>
2020-10-27 16:41:35 +0100

    Chapter 3 exercises

--- Introduction Java Programming/MyWork/src/Chapter 03 Selections/ProgrammingExcercises/NumberOfDaysInMonth.java
+++
@@ -1,9 +0,0 @@
-package com.company.Chapter3Selections.ProgrammingExcercises;
-
-public class NumberOfDaysInMonth {
-    /* Write a program that prompts the user to enter the month and year and displays the number of days in the month.
-    Example: if the user inputs 2 and 2012, the program should display that February 2012 has 29 days.
-    Example: if the user inputs 3 and 2015, the program should display that March 2015 has 31 days.
-     */
-
-}
