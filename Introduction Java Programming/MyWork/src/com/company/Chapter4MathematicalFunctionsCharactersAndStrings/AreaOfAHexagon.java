c8a2935dd9598d392a94069b51de59bb205c0a99
Kim Van de Casteele <kim.vandecasteele@sfpd.fgov.be>
2020-11-04 16:33:57 +0100

    Characters Summary Prep exercises

---
+++ Introduction Java Programming/MyWork/src/com/company/Chapter4MathematicalFunctionsCharactersAndStrings/AreaOfAHexagon.java
@@ -0,0 +1,12 @@
+package com.company.Chapter4MathematicalFunctionsCharactersAndStrings;
+
+public class AreaOfAHexagon {
+    /*  The area of a hexagon can be computed using the following formula (s is the length of a side):
+            area = (6 * pow(x,2)) / (4 * tan(PI / 6))
+        Write a program that prompts the user to enter the side of a hexagon and displays its area.
+        Sample run:
+            Enter the side: 5,5
+            The area of the hexagon is 78,59
+     */
+
+}
---
+++ Introduction Java Programming/MyWork/src/com/company/Chapter4MathematicalFunctionsCharactersAndStrings/AreaOfARegularPolygon.java
@@ -0,0 +1,15 @@
+package com.company.Chapter4MathematicalFunctionsCharactersAndStrings;
+
+public class AreaOfARegularPolygon {
+    /*  A regular polygon is an n-sided polygon in which all sides are of the same length and all angles have the same
+        degree. The formula for computing the area of a regular polygon is
+            area = (n * pow(s,2)) / (4 * tan(PI / n))
+        Here, s is the length of a side. Write a program that prompts the user to enter the number of sides and their
+        length of a regular polygon and displays its area.
+        Sample run:
+            Enter the number of sides: 5
+            Enter the side: 6,5
+            The area of the polygon is 72,69017017488385
+     */
+
+}
