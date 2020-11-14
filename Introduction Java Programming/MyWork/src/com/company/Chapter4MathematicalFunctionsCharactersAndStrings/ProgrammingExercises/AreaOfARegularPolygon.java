c12af2fcb8df8e00bf036bc559bf2caf87ada4ca
Kim Van de Casteele <Kinkyk013@gmail.com>
2020-11-14 20:05:17 +0100

    added 4 exercises of chapter 4

--- Introduction Java Programming/MyWork/src/com/company/Chapter4MathematicalFunctionsCharactersAndStrings/ProgrammingExercises/AreaOfARegularPolygon.java
+++ Introduction Java Programming/MyWork/src/com/company/Chapter4MathematicalFunctionsCharactersAndStrings/ProgrammingExercises/AreaOfARegularPolygon.java
@@ -1,5 +1,7 @@
 package com.company.Chapter4MathematicalFunctionsCharactersAndStrings.ProgrammingExercises;
 
+import java.util.Scanner;
+
 public class AreaOfARegularPolygon {
     /*  A regular polygon is an n-sided polygon in which all sides are of the same length and all angles have the same
         degree. The formula for computing the area of a regular polygon is
@@ -11,5 +13,13 @@ public class AreaOfARegularPolygon {
             Enter the side: 6,5
             The area of the polygon is 72,69017017488385
      */
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of sides: ");
+        int n = input.nextInt();
+        System.out.println("Enter the length of the side: ");
+        double s = input.nextDouble();
+        double area = (n * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / n));
+        System.out.println("The area of the polygon is " + area);
+    }
 }
