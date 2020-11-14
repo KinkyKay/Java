d074d909376e70cbabad9d8c4343bb35da51f535
Kim Van de Casteele <Kinkyk013@gmail.com>
2020-11-14 20:05:17 +0100

    added 4 exercises of chapter 4

--- Introduction Java Programming/MyWork/src/com/company/Chapter4MathematicalFunctionsCharactersAndStrings/ProgrammingExercises/AreaOfAPentagon.java
+++ Introduction Java Programming/MyWork/src/com/company/Chapter4MathematicalFunctionsCharactersAndStrings/ProgrammingExercises/AreaOfAPentagon.java
@@ -1,5 +1,7 @@
 package com.company.Chapter4MathematicalFunctionsCharactersAndStrings.ProgrammingExercises;
 
+import java.util.Scanner;
+
 public class AreaOfAPentagon {
     /* Write a program that prompts the user to enter the length from the center of a pentagon to a vertex (r) and
        computes the area of the pentagon.
@@ -10,5 +12,12 @@ public class AreaOfAPentagon {
             Enter the length from the center to a vertex: 5,5
             The area of the pentagon is 71,92
      */
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length from the center to a vertex: ");       // displays question
+        double r = input.nextDouble();                                              // vertex
+        double s = (2 * r) * (Math.sin(Math.PI / 5));                               // compute side
+        double area = (5 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / 5));           // compute area
+        System.out.printf("The area of the pentagon is %4.2f",area);                // displays result
+    }
 }
